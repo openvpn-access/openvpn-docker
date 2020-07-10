e39713a18dd43cb48a384b4f47d5d34e85b9f136
Nathan Seymour <nathan@seymour.global>
2020-07-10 15:50:20 +0200

    Add config and setup scripts

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,22 @@
 FROM ubuntu:18.04
+
+# Install dependencies
+RUN apt update -y \
+    && apt upgrade -y \
+    && apt install openvpn openssl -y
+
+COPY . /opt/app
+WORKDIR /opt/app
+
+RUN mkdir /etc/openvpn/pki
+
+EXPOSE 1194
+
+RUN chmod +x docker-entrypoint.sh
+ENTRYPOINT ./docker-entrypoint.sh \
+                "$REQ_COUNTRY" \
+                "$REQ_PROVINCE" \
+                "$REQ_CITY" \
+                "$REQ_ORG" \
+                "$REQ_OU" \
+                "$REQ_CN"
---
+++ docker-entrypoint.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+cd /etc/openvpn/pki
+
+if [[ ! -f ./private/openvpn-access.pem ]]; then
+    # Setup the PKI
+    mkdir -p private req crt
+
+    # Need to to do this because without it, openssl throws errors
+    touch /root/.rnd
+
+    echo "Generating private key..."
+    openssl genrsa -out private/openvpn-access.pem 4096
+
+    echo "Generating main request..."
+    openssl req -nodes -new -key private/openvpn-access.pem -out req/openvpn-access.csr -subj "/C=$1/ST=$2/L=$3/O=$4/OU=$5/CN=$6"
+
+    echo "Generating CA public key..."
+    openssl genrsa -out private/ca.pem 4096
+
+    echo "Generating CA request..."
+    openssl req -nodes -new -x509 -key private/ca.pem -out crt/ca.crt -subj "/CN=OpenVPN-Access Certificate Authority"
+
+    echo "Signing vpn request..."
+    openssl x509 -req -in req/openvpn-access.csr -CA crt/ca.crt -CAkey private/ca.pem -CAcreateserial -out crt/openvpn-access.crt
+
+    echo "Generating diphie hellman params..."
+    openssl dhparam -out dh.pem 4096 &> /dev/null
+
+    echo "Generatinng ta secret..."
+    openvpn --genkey --secret ta.key
+else
+    echo "PKI already exists!"
+fi
+
+mkdir -p /dev/net
+if [ ! -c /dev/net/tun ]; then
+    mknod /dev/net/tun c 10 200
+fi
+
+cd /etc/openvpn
+
+echo "Copying configuration..."
+cp /opt/app/server.conf .
+
+echo "Configuring networking rules..."
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sysctl -p
+
+echo "Starting OpenVPN server..."
+/usr/sbin/openvpn --cd /etc/openvpn --script-security 2 --config /etc/openvpn/server.conf
